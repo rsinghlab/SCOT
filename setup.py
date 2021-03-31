29013896abb3e01f17d02847a4b1af58d8d54924
Alex Tong <alexanderytong@gmail.com>
2021-03-30 21:24:32 -0400

    Add correct version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="SCOT",
-    version="0.1.0",
+    version="0.2.1",
     description="Gromov-Wasserstein optimal transport for aligning single-cell multi-omics data",
     author="Pinar Demetci",
     packages=find_packages(),
